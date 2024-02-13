01bf24e8a85bf037f5c9cbfd2b376daf2075d111
SaniyaNasreen <136255169+SaniyaNasreen@users.noreply.github.com>
2024-02-13 09:12:14 +0530

    last commit

--- models/productmodel.js
+++ models/productmodel.js
@@ -60,10 +60,6 @@ const productSchema = mongoose.Schema({
     type: Boolean,
     default: true,
   },
-  couponApplied: {
-    type: Boolean,
-    default: false,
-  },
 });
 
 module.exports = mongoose.model("Product", productSchema);
